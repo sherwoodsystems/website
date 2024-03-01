bed7ae95baa994e3288a8b3b0ff28e2af05e77ab
Cameron Slipp <cdslipp@gmail.com>
2024-03-01 13:59:01 -0500

    Trying to fix forms

--- src/routes/production/contact/+page.svelte
+++ src/routes/production/contact/+page.svelte
@@ -1,25 +1,32 @@
 <script>
-	let eventType = '';
+	import { onMount } from 'svelte';
+	let showIframe = false;
+
+	onMount(() => {
+		showIframe = true;
+	});
 </script>
 
-<div class="container">
-	<article>
-		<iframe
-			width="1000px"
-			height="1000px"
-			src="https://forms.office.com/Pages/ResponsePage.aspx?id=1hrLd-iLI0uIYc9rJNhg_UcPwrHrcBlAmxQaKYx0pVFUQU9TMzdLTzE5NjMxUExHTUlEWldVNE9QWi4u&embed=true"
-			frameborder="0"
-			marginwidth="0"
-			marginheight="0"
-			style="border: none; max-width:100%; max-height:100vh"
-			allowfullscreen
-			webkitallowfullscreen
-			mozallowfullscreen
-			msallowfullscreen
-		>
-		</iframe>
-	</article>
-</div>
+{#if showIframe}
+	<div class="container">
+		<article>
+			<iframe
+				width="1000px"
+				height="1000px"
+				src="https://forms.office.com/Pages/ResponsePage.aspx?id=1hrLd-iLI0uIYc9rJNhg_UcPwrHrcBlAmxQaKYx0pVFUQU9TMzdLTzE5NjMxUExHTUlEWldVNE9QWi4u&embed=true"
+				frameborder="0"
+				marginwidth="0"
+				marginheight="0"
+				style="border: none; max-width:100%; max-height:100vh"
+				allowfullscreen
+				webkitallowfullscreen
+				mozallowfullscreen
+				msallowfullscreen
+			>
+			</iframe>
+		</article>
+	</div>
+{/if}
 
 <style>
 	.container {
