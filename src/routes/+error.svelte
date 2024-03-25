4d5c436a8fd4faf84d2e2e380c9a20aff6c833c1
Cameron Slipp <cdslipp@gmail.com>
2024-03-25 12:48:10 -0400

    Adding robots.txt and error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<div class="container">
+  <article>
+    <h1>Error</h1>
+    <p>
+      We're sorry, something went wrong. The page you're looking for might not
+      exist.
+    </p>
+  </article>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  article {
+    text-align: center;
+    min-width: 60dvw;
+  }
+</style>
